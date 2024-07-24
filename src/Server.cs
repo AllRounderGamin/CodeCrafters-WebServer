6c1537ff4f2750b2af749163864c30be39d072ef
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2024-07-24 04:11:47 +0100

    Stage 2

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
+TcpListener server = new(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+Socket socket = server.AcceptSocket(); // wait for client
+socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
