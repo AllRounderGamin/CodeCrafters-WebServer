4addb568970cad00c5bea90dce3745f7db5d511d
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2024-07-28 01:14:37 +0100

    Stage 4

--- src/Server.cs
+++ src/Server.cs
@@ -11,11 +11,16 @@ byte[] buffer = new byte[1_024];
 int received = await socket.ReceiveAsync(buffer);
 string message = Encoding.UTF8.GetString(buffer);
 string[] data = message.Split("\r\n");
-string requestedURL = data[0].Split(" ")[1];
+string[] requestedURL = data[0].Split(" ")[1].Split("/");
 
-switch (requestedURL){
-    case "/":
+
+switch (requestedURL[1]){
+    case "":
         await socket.SendAsync(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+        break;
+    case "echo":
+        string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + requestedURL[2].Length.ToString() + "\r\n\r\n" + requestedURL[2];
+        await socket.SendAsync(Encoding.UTF8.GetBytes(response));
         break;
     default:
         await socket.SendAsync(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
@@ -23,3 +28,7 @@ switch (requestedURL){
 }
 socket.Close();
 }
+
+
+
+// curl -i -X GET http://localhost:4221/
