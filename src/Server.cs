86a69779e9addd125f93d0d8d85bbaa1222fbf74
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2024-07-30 01:11:27 +0100

    Stage 7

--- src/Server.cs
+++ src/Server.cs
@@ -23,14 +23,23 @@ async static void handleConn(Socket socket){
             await socket.SendAsync(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
             break;
         case "echo":
-            string echoResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + requestedURL[2].Length.ToString() + "\r\n\r\n" + requestedURL[2];
+            string echoResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + requestedURL[2].Length + "\r\n\r\n" + requestedURL[2];
             await socket.SendAsync(Encoding.UTF8.GetBytes(echoResponse));
             break;
         case "user-agent":
             int agentIndex = Array.FindIndex(data, str => str.StartsWith("User-Agent", StringComparison.InvariantCultureIgnoreCase));
             string userAgent = data[agentIndex].Split(":")[1].Trim();
-            string agentResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.Length.ToString() + "\r\n\r\n" + userAgent;
+            string agentResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.Length + "\r\n\r\n" + userAgent;
             await socket.SendAsync(Encoding.UTF8.GetBytes(agentResponse));
+            break;
+        case "files":
+            string fileUrl = "./tmp/" + requestedURL[2];
+            if (!File.Exists(fileUrl)){
+                await socket.SendAsync(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
+            }
+            string fileContent = File.ReadAllText(fileUrl);
+            string fileResponse = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + fileContent.Length + "\r\n\r\n" + fileContent;
+            await socket.SendAsync(Encoding.UTF8.GetBytes(fileResponse));
             break;
         default:
             await socket.SendAsync(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
